1512ed113e09b178e2e826e7fe161caaf939d738
nimishag2000 <nimishag2000@yahoo.com>
2013-09-18 18:47:42 +0530

    version1

---
+++ src/vision/soft/test/SampleActivity.java
@@ -0,0 +1,14 @@
+package vision.soft.test;
+
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class SampleActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
